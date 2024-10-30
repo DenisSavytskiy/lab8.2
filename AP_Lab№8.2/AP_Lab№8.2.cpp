b04872f8a6fac5f3c973dffd1f4e8f56183546ff
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-30 02:44:11 +0200

    first commit

---
+++ AP_Lab№8.2/AP_Lab№8.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+size_t CountWords(const string& s)
+{
+    size_t wordCount = 0;
+    bool inWord = false;
+
+    for (size_t i = 0; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            if (!inWord)
+            {
+                wordCount++;
+                inWord = true;
+            }
+        }
+        else
+        {
+            inWord = false;
+        }
+    }
+
+    return wordCount;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    size_t wordCount = CountWords(str);
+    cout << "Number of words in string: " << wordCount << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include <pch.h>
+#include "CppUnitTest.h"
+#include "../AP_Lab№8.2/AP_Lab№8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace WordCounterTests
+{
+    TEST_CLASS(WordCounterTests)
+    {
+    public:
+        TEST_METHOD(TestMultipleSpaces)
+        {
+            string input = "One   two     three";
+            size_t result = CountWords(input);
+            Assert::AreEqual(size_t(3), result);
+        }
+    };
+}
